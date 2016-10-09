3bf7e99262c88d767f432c9e29dc133043f69ed9
madcap28 <madcap28146@gmail.com>
2016-10-09 18:09:27 -0400

    First Test

---
+++ TicketSystem/Models/Ticket.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TicketSystem.Models
+{
+    public class Ticket
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime Date { get; set; }
+        public string Email { get; set; }
+        public string Status { get; set; }
+        public String CustomerResponse { get; set; }
+        public String EmployeeResponse { get; set; }
+        public string ScheduledDay { get; set; }
+        public string ScheduledTime { get; set; }
+    }
+}
---
+++ TicketSystem/Startup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.Owin;
+using Owin;
+using TicketSystem.Models;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.AspNet.Identity;
+using System.Security.Claims;
+
+[assembly: OwinStartupAttribute(typeof(TicketSystem.Startup))]
+namespace TicketSystem
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            createUsersAndRoles();
+        }
+
+        private void createUsersAndRoles()
+        {
+            ApplicationDbContext context = new ApplicationDbContext();
+
+            var roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(context));
+            var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+
+
+            if (!roleManager.RoleExists("Admin"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Admin";
+                roleManager.Create(role);
+            }
+            if (!roleManager.RoleExists("Employee"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Employee";
+                roleManager.Create(role);
+
+            }
+
+            if (!roleManager.RoleExists("Customer"))
+            {
+                var role = new IdentityRole();
+                role.Name = "Customer";
+                roleManager.Create(role);
+
+            }
+        }
+    }
+
+    
+}
