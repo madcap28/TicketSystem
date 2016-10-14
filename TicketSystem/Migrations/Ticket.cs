62790a0b8de279a38f5e529b2a282874883dad35
madcap28 <madcap28146@gmail.com>
2016-10-14 08:56:24 -0400

    Changed to Code First Database

---
+++ TicketSystem/Migrations/Ticket.cs
@@ -0,0 +1,31 @@
+namespace TicketSystem.Migrations
+{
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel.DataAnnotations;
+    using System.ComponentModel.DataAnnotations.Schema;
+    using System.Data.Entity.Spatial;
+
+    public partial class Ticket
+    {
+        public int ID { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string Email { get; set; }
+
+        public string Status { get; set; }
+
+        public string CustomerResponse { get; set; }
+
+        public string EmployeeResponse { get; set; }
+
+        public string ScheduledTime { get; set; }
+
+        public string ScheduledDay { get; set; }
+    }
+}
