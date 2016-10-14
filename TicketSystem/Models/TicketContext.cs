48d4f0c2a3bd101ac5430b3d0ffc3c718aea2447
madcap28 <madcap28146@gmail.com>
2016-10-14 11:51:26 -0400

    Changed Override Method Model Builder

--- TicketSystem/Models/TicketContext.cs
+++ TicketSystem/Models/TicketContext.cs
@@ -5,29 +5,18 @@ namespace TicketSystem.Models
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
     using Microsoft.AspNet.Identity.EntityFramework;
-    using System.Security.Claims;
-    using System.Threading.Tasks;
-    using Microsoft.AspNet.Identity;
-    public class ApplicationUser : IdentityUser
-    {
-        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
-        {
-            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
-            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
-            // Add custom user claims here
-            return userIdentity;
-        }
-    }
+
     public partial class TicketContext : IdentityDbContext<ApplicationUser>
     {
         public TicketContext()
-            : base("AppHarborConnection", throwIfV1Schema : false)
+            : base("AppHarborConnection", throwIfV1Schema:false)
         {
         }
         public static TicketContext Create()
         {
             return new TicketContext();
         }
+
         public virtual DbSet<C__MigrationHistory> C__MigrationHistory { get; set; }
         public virtual DbSet<AspNetRole> AspNetRoles { get; set; }
         public virtual DbSet<AspNetUserClaim> AspNetUserClaims { get; set; }
@@ -51,6 +40,9 @@ namespace TicketSystem.Models
                 .HasMany(e => e.AspNetUserLogins)
                 .WithRequired(e => e.AspNetUser)
                 .HasForeignKey(e => e.UserId);
+            modelBuilder.Entity<IdentityRole>().HasKey<string>(r => r.Id).Property(p => p.Name).IsRequired();
+            modelBuilder.Entity<IdentityUserRole>().HasKey(r => new { r.RoleId, r.UserId });
+            modelBuilder.Entity<IdentityUserLogin>().HasKey(u => new { u.UserId, u.LoginProvider, u.ProviderKey });
         }
     }
 }
