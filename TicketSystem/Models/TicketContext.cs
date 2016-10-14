a1d3b48388331c5a708980c1403e8a0c3c310f13
madcap28 <madcap28146@gmail.com>
2016-10-14 11:58:03 -0400

    Moved ApplicationUser

--- TicketSystem/Models/TicketContext.cs
+++ TicketSystem/Models/TicketContext.cs
@@ -5,7 +5,20 @@ namespace TicketSystem.Models
     using System.ComponentModel.DataAnnotations.Schema;
     using System.Linq;
     using Microsoft.AspNet.Identity.EntityFramework;
+    using System.Security.Claims;
+    using System.Threading.Tasks;
+    using Microsoft.AspNet.Identity;
 
+    public class ApplicationUser : IdentityUser
+    {
+        public async Task<ClaimsIdentity> GenerateUserIdentityAsync(UserManager<ApplicationUser> manager)
+        {
+            // Note the authenticationType must match the one defined in CookieAuthenticationOptions.AuthenticationType
+            var userIdentity = await manager.CreateIdentityAsync(this, DefaultAuthenticationTypes.ApplicationCookie);
+            // Add custom user claims here
+            return userIdentity;
+        }
+    }
     public partial class TicketContext : IdentityDbContext<ApplicationUser>
     {
         public TicketContext()
